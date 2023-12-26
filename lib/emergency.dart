b6b206a29de4e2303387193f56e2c52a6844a5b6
reezonstha <140035178+reezonstha@users.noreply.github.com>
2023-12-26 10:15:30 +0545

    Create emergency.dart

---
+++ lib/emergency.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+import 'package:health_app/newpage.dart';
+
+class Emergency extends StatelessWidget {
+  // const Report({super.key});
+  TextEditingController username = TextEditingController();
+  TextEditingController password = TextEditingController();
+
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(
+        child: Column(
+          children: [
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Text("Emergency number: +9779846691312"),
+            ),
+            ElevatedButton(onPressed: (){
+              Navigator.pop(context, MaterialPageRoute(builder: (context) => NewPage(username.text,password.text)));
+            },
+                child: Text("Submit")),
+          ],
+        ),
+      ),
+    );
+  }
+}
